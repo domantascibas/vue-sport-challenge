11e529d5f7b78d7a6b460a4c41389ae7e4f8de8a
Domantas Cibas <domantas@nectarsun.com>
2020-01-05 01:24:55 +0200

    sign up new users to firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import firebase from 'firebase'
+import "firebase/firestore"
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+if (firebase.apps.length === 0) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAtcwX-6UZGe1FYRdGK-uFma6Ibqvkixps",
+        authDomain: "sports-challenge.firebaseapp.com",
+        databaseURL: "https://sports-challenge.firebaseio.com",
+        projectId: "sports-challenge",
+        storageBucket: "sports-challenge.appspot.com",
+        messagingSenderId: "961864914058",
+        appId: "1:961864914058:web:575533d99853caf806c973"
+    };
+    firebase.initializeApp(firebaseConfig);
+}
 
 new Vue({
   router,
