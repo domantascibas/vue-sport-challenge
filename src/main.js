5c06f0aa55e77989c8a323de9b6327a480cab659
Domantas Cibas <domantas@nectarsun.com>
2020-01-05 02:37:41 +0200

    firebase web deploy

--- src/main.js
+++ src/main.js
@@ -9,15 +9,15 @@ Vue.config.productionTip = false
 let app = ''
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAtcwX-6UZGe1FYRdGK-uFma6Ibqvkixps",
-    authDomain: "sports-challenge.firebaseapp.com",
-    databaseURL: "https://sports-challenge.firebaseio.com",
-    projectId: "sports-challenge",
-    storageBucket: "sports-challenge.appspot.com",
-    messagingSenderId: "961864914058",
-    appId: "1:961864914058:web:575533d99853caf806c973"
-})
-
+    "projectId": "sports-challenge",
+    "appId": "1:961864914058:web:575533d99853caf806c973",
+    "databaseURL": "https://sports-challenge.firebaseio.com",
+    "storageBucket": "sports-challenge.appspot.com",
+    "locationId": "europe-west",
+    "apiKey": "AIzaSyAtcwX-6UZGe1FYRdGK-uFma6Ibqvkixps",
+    "authDomain": "sports-challenge.firebaseapp.com",
+    "messagingSenderId": "961864914058"
+});
 
 firebase.auth().onAuthStateChanged(() => {
     if (!app) {
